e04a5a1f2ecf04500e820838be4d1133abf239a6
SabeloMkhwanzi <sabelo.mailme@gmail.com>
2022-12-18 17:29:51 +0200

    update landing page

--- components/HomePage/index.tsx
+++ components/HomePage/index.tsx
@@ -137,8 +137,8 @@ export default function HomePage() {
             </h1>
 
             <Text fz={24} className={Jaldi.className} color="#8892B0">
-              I’m a software developer with passion for building amazing Apps, A
-              web3 Enthusiast - hacking and building dapps that promotes
+              I’m a software developer with passion for building amazing Apps, a
+              Web3 enthusiast - hacking and building dapps that promotes
               decentralization.
             </Text>
 
